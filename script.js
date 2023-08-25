8ae1ea2b7b61105d41375eabcf9f6b8c5de7d5da
VaishnaviHonale <137199492+VaishnaviHonale@users.noreply.github.com>
2023-08-25 12:22:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 window.promises = [];
+let pro1=new Promise((reslove,reject)=>{
+	setTimeout(()=>{
+		reslove("Pro1");
+	},Math.floor(Math.random()*5000)+1000);
+});
+let pro2=new Promise((reslove,reject)=>{
+	setTimeout(()=>{
+		reslove("pro2");
+		
+	},Math.floor(Math.random()*5000)+1000);
+});
+let pro3=new Promise((reslove,reject)=>{
+	setTimeout(()=>{
+		reslove("pro3");
+		
+	},Math.floor(Math.random()*5000)+1000);
+});
+let pro4=new Promise((reslove,reject)=>{
+	setTimeout(()=>{
+		reslove("pro4");
+		
+	},Math.floor(Math.random()*5000)+1000);
+});
+let pro5=new Promise((reslove,reject)=>{
+	setTimeout(()=>{
+		reslove("pro5");
+		
+	},Math.floor(Math.random()*5000)+1000);
+});
+const myArray=[pro1,pro2,pro3,pro4,pro5];
+promise.any(myArray).then((result)=>{
+	document.getElementById("output").innerText=result;
+});
+
 
 // Do not change the code above this
 // add your promises to the array `promises`
